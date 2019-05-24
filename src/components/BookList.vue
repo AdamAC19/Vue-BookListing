5b8debf9cd722fa16ffc9df2f77819e7762866f4
AdamAC19 <adam.cooper@1ststop.co.uk>
2019-05-24 14:43:23 +0100

    book names updated

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -14,9 +14,9 @@ export default {
     return {
       title: "All Books",
       books: [
-          { title: "book 1", author: "author1" },
-          {title: 'book 2', author: 'author2'},
-          {title: 'book 3', author: 'author3'}
+          {title: "Self-Reliance", author: "Ralph Waldo Emerson" },
+          {title: 'American Gods', author: 'Neil Gaiman'},
+          {title: 'Amusing Ourselves to Death', author: 'Neil Postman'}
       ]
     };
   }

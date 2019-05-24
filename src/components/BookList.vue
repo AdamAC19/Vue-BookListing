6e827824208f6274148019e41692456f94711cd6
AdamAC19 <adam.cooper@1ststop.co.uk>
2019-05-24 14:41:46 +0100

    v-for loop

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,22 +1,39 @@
 <template>
-    <div>
-        <h1>{{title}}</h1>
-    </div>
+  <div>
+    <h1>{{title}}</h1>
+    <ul>
+      <li v-for="book in books">{{book.title}}: {{book.author}}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'BookList',
-    data() {
-        return {
-            title: 'All Books'
-        }
-    }
-}
+  name: "BookList",
+  data() {
+    return {
+      title: "All Books",
+      books: [
+          { title: "book 1", author: "author1" },
+          {title: 'book 2', author: 'author2'},
+          {title: 'book 3', author: 'author3'}
+      ]
+    };
+  }
+};
 </script>
 
 <style>
-    h1, h2 {
-        font-weight: normal
-    }
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: block;
+  margin: 0 10px;
+}
 </style>
